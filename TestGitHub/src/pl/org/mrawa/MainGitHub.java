5272ad7be87b020c4c965b49bf472b45ec5c9868
Marcin Rawa <Martin.Rawa@gmail.com>
2014-05-04 19:18:52 +0200

    first  git hub commit

---
+++ TestGitHub/src/pl/org/mrawa/MainGitHub.java
@@ -0,0 +1,15 @@
+package pl.org.mrawa;
+
+public class MainGitHub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello in git world");
+
+	}
+
+}
