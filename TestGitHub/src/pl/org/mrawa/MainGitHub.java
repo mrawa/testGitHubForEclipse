c090b831249187d9f191c6fa82d480fc8785f9cd
Marcin Rawa <Martin.Rawa@gmail.com>
2014-05-04 19:49:36 +0200

    add comment

--- TestGitHub/src/pl/org/mrawa/MainGitHub.java
+++ TestGitHub/src/pl/org/mrawa/MainGitHub.java
@@ -2,13 +2,11 @@ package pl.org.mrawa;
 
 public class MainGitHub {
 
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+
 		System.out.println("Hello in git world");
+		//TODO 
 
 	}
 
